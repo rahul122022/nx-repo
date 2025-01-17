caddc9f9435c2093ac632554f7768772929c3186
Rahul Prajapati <rahul.prajapati@capgemini.com>
2025-01-17 17:19:16 +0530

    Add splitter

--- apps/rahul-test/src/app/app.component.ts
+++ apps/rahul-test/src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AgGridPocComponent } from '@rahul/ag-grid';
+import { ArcGisPocComponent } from '@rahul/arc-gis';
+import { AngularSplitModule } from 'angular-split';
 
 @Component({
-  imports: [AgGridPocComponent, RouterModule],
+  imports: [
+    AgGridPocComponent,
+    ArcGisPocComponent,
+    RouterModule,
+    AngularSplitModule,
+  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
