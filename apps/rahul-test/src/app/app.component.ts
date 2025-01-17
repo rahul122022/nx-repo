4c9d39f2eb962f81e8ecf8fcc9542c3bbf09aa81
Rahul Prajapati <rahul.prajapati@capgemini.com>
2025-01-17 15:00:30 +0530

    Add ag grid in lib

--- apps/rahul-test/src/app/app.component.ts
+++ apps/rahul-test/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { AgGridPocComponent } from '@rahul/ag-grid';
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [AgGridPocComponent, RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
