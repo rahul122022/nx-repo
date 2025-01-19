21a21deb9c81af433c72d5d7794b0c4dac91ea0b
Rahul Prajapati <rahulcse.1990@gmail.com>
2025-01-19 19:06:40 +0530

    Fix component name

--- libs/charts-poc/src/lib/line-chart/line-chart.component.ts
+++ libs/charts-poc/src/lib/line-chart/line-chart.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'lib-d3-chart',
+  selector: 'lib-d3-line-chart',
   imports: [CommonModule],
   templateUrl: './line-chart.component.html',
   styleUrl: './line-chart.component.scss',
 })
-export class D3ChartComponent {}
+export class D3LineChartComponent {}
