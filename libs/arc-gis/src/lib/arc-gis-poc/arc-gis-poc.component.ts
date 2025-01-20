f507d1ddf9d55e4799149bd546317f25234b923f
Aditi-Pritam <kashyapaditipritam@gmail.com>
2025-01-20 15:13:56 +0530

    arcgis-map component updated

--- libs/arc-gis/src/lib/arc-gis-poc/arc-gis-poc.component.ts
+++ libs/arc-gis/src/lib/arc-gis-poc/arc-gis-poc.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from "@angular/core";
+import { setAssetPath as setCalciteComponentsAssetPath } from '@esri/calcite-components/dist/components';
+import "@arcgis/map-components/dist/components/arcgis-map";
+import "@arcgis/map-components/dist/components/arcgis-zoom";
+import "@arcgis/map-components/dist/components/arcgis-placement";
+import "@arcgis/map-components/dist/components/arcgis-expand";
+import "@arcgis/map-components/dist/components/arcgis-legend";
 
 @Component({
   selector: 'lib-arc-gis-poc',
-  imports: [CommonModule],
   templateUrl: './arc-gis-poc.component.html',
   styleUrl: './arc-gis-poc.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class ArcGisPocComponent {}
+export class ArcGisPocComponent {
+  constructor() {
+    setCalciteComponentsAssetPath("https://js.arcgis.com/calcite-components/2.13.2/assets");
+  }
+
+  // arcgisViewChange(event: CustomEvent) {
+  //   const { center, zoom } = event.target;
+  //   console.log("Center (lon/lat): ", `${center.longitude}, ${center.latitude}`);
+  //   console.log("Zoom: ", zoom);
+  // }
+
+  handleClick(){
+    console.log("Hello Aditi");
+  }
+}
