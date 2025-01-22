30cb9c37707374f9a3d1dcecb73e7b5d52344f49
Rahul Prajapati <rahul.prajapati@capgemini.com>
2025-01-22 21:44:41 +0530

    Add API

--- libs/arc-gis/src/lib/arc-gis-poc/arc-gis-poc.component.ts
+++ libs/arc-gis/src/lib/arc-gis-poc/arc-gis-poc.component.ts
@@ -1,10 +1,15 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from "@angular/core";
-import { setAssetPath as setCalciteComponentsAssetPath } from '@esri/calcite-components/dist/components';
-import "@arcgis/map-components/dist/components/arcgis-map";
-import "@arcgis/map-components/dist/components/arcgis-zoom";
-import "@arcgis/map-components/dist/components/arcgis-placement";
-import "@arcgis/map-components/dist/components/arcgis-expand";
-import "@arcgis/map-components/dist/components/arcgis-legend";
+import {
+  AfterViewInit,
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  ViewChild,
+} from '@angular/core';
+
+import EsriMap from '@arcgis/core/Map';
+import EsriMapView from '@arcgis/core/views/MapView';
+import Track from "@arcgis/core/widgets/Track";
+import * as reactiveUtils from "@arcgis/core/core/reactiveUtils";
+
 
 @Component({
   selector: 'lib-arc-gis-poc',
@@ -12,9 +17,52 @@ import "@arcgis/map-components/dist/components/arcgis-legend";
   styleUrl: './arc-gis-poc.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class ArcGisPocComponent {
-  constructor() {
-    setCalciteComponentsAssetPath("https://js.arcgis.com/calcite-components/2.13.2/assets");
+export class ArcGisPocComponent implements AfterViewInit {
+  @ViewChild('eventMap', { static: true }) private mapViewEl: any;
+
+  ngAfterViewInit(): void {
+    this.initializeMap();
+  }
+
+  initializeMap() {
+    const mapProperties = {
+      basemap: 'topo-vector',
+    };
+
+    const map = new EsriMap(mapProperties);
+    const mapViewProperties = {
+      container: this.mapViewEl.nativeElement,
+      center: [-101.41, 40.78],
+      // center: [21.7679, 78.8718],
+      zoom: 7,
+      navigation: {
+        gamepad: {
+          enabled: false,
+        },
+      },
+      map: map,
+      popup: {
+        collapseEnabled: false,
+        autoOpenEnabled: false,
+        dockEnabled: false,
+        dockOptions: {
+          // Disables the dock button from the popup
+          buttonEnabled: true,
+          // Ignore the default sizes that trigger responsive docking
+          breakpoint: false,
+          position: 'bottom-center',
+        },
+      },
+    };
+
+    const mapView: any = new EsriMapView(mapViewProperties);
+// adding track 
+    const tracks = new Track({
+      view: mapView
+    });
+    mapView.ui.add(tracks, 'top-left');
+
+    // reactiveUtils.whenOnce(mapView, "ready"). then() => {};
   }
 
   // arcgisViewChange(event: CustomEvent) {
@@ -22,8 +70,4 @@ export class ArcGisPocComponent {
   //   console.log("Center (lon/lat): ", `${center.longitude}, ${center.latitude}`);
   //   console.log("Zoom: ", zoom);
   // }
-
-  handleClick(){
-    console.log("Hello Aditi");
-  }
 }
