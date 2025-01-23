97a063861903a694048fe5d0aed39b7840af840b
subodhyadav123 <subodh-birendra.yadav@capgemini.com>
2025-01-23 15:35:58 +0530

    Adding initial arcgis files

---
+++ libs/arc-gis/src/lib/arcgis-subodh/arcgis-subodh.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-arcgis-subodh',
+  imports: [],
+  templateUrl: './arcgis-subodh.component.html',
+  styleUrl: './arcgis-subodh.component.css'
+})
+export class ArcgisSubodhComponent {
+
+}
